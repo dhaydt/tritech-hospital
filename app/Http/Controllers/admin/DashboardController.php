5ead79cbf3a5cb641420dabd564175c021acb916
dhaydt <1992dayat@gmail.com>
2022-02-05 00:32:12 +0700

    Revert "dashboard view"
    This reverts commit b20e9eb638a9f07bbf3f8d76bd42466ea91867db.

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -3,10 +3,6 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\Admin;
-use App\Models\Checkup;
-use App\Models\Content;
-use App\Models\Customer;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 class DashboardController extends Controller
@@ -18,12 +14,8 @@ class DashboardController extends Controller
 
     public function dashboard()
     {
-        $pasien = Customer::orderby('created_at', 'DESC')->get();
-        $checkup = Checkup::orderby('created_at', 'DESC')->get();
-        $content = Content::orderby('created_at', 'DESC')->get();
-        $admin = Admin::orderby('created_at', 'DESC')->get();
         session()->put('title', 'Dashboard');
 
-        return view('admin-views.system.dashboard', compact('pasien', 'checkup', 'content', 'admin'));
+        return view('admin-views.system.dashboard');
     }
 }
